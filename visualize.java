8f022f7ff4d42c9cbd338f3d022136746633f69f
Periyasamy V <periguruaravi@gmail.com>
2021-03-02 11:26:20 +0530

    initial
    creation of base class

---
+++ visualize.java
@@ -0,0 +1,14 @@
+//this is the place for adding images
+import java.io.*;
+import javax.swing.*;
+public class visualize
+{
+	public static void main(String args[])
+	{
+		JFrame frame= new JFrame("visualize");
+		frame.setSize(500,500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
+
