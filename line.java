9ac8ee80466653270329feb9b07a0f1dfae7b800
Periyasamy V <periguruaravi@gmail.com>
2021-03-08 21:46:17 +0530

    Create line.java
    new class for line is created

---
+++ line.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class line
+{
+	point p1,p2;
+	public line(int x1,int y1,int z1,int x2,int y2,int z2)
+	{
+		p1=new point(x1,y1,z1);
+		p2=new point(x2,y2,z2);
+	}
+	public void main(String args[])
+	{
+		line l1=new line(x1,y1,z1,x2,y2,z2);
+		
+	}
+	
+}
